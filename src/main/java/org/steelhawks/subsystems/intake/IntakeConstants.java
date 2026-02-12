28b38518bf0c50ce038e26fbc4399140035732a8
krishp <parmar1coding@gmail.com>
2026-02-12 11:25:43 -0500

    started slot configs for Intake TalonFX

--- src/main/java/org/steelhawks/subsystems/intake/IntakeConstants.java
+++ src/main/java/org/steelhawks/subsystems/intake/IntakeConstants.java
@@ -9,8 +9,9 @@ public class IntakeConstants {
     public static final int EXTENSION_LEFT_MOTOR_ID = 0;
     public static final int EXTENSION_RIGHT_MOTOR_ID = 1;
     public static final TunableNumber EXTENSION_GEAR_RATIO = new TunableNumber("Extension Gear Ratio", 4 / 1);
+    public static final int EXTENSION_CANBUS_ID = 3;
 
-    public static final double ROLLER_MOTOR_ID = 0;
+    public static final int ROLLER_MOTOR_ID = 0;
 
     public static final double PINION_RADIUS = Units.inchesToMeters(1.033922 / 2.0);
     public static final double PINION_DIAMETER = PINION_RADIUS / 2.0;
--- src/main/java/org/steelhawks/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/org/steelhawks/subsystems/intake/IntakeIOTalonFX.java
@@ -1,7 +1,16 @@
 package org.steelhawks.subsystems.intake;
 
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.Slot1Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.units.measure.*;
 
 
@@ -29,6 +38,49 @@ public class IntakeIOTalonFX implements IntakeIO {
     private final StatusSignal<Current> rollerCurrentAmps;
     private final StatusSignal<Voltage> rollerAppliedVoltage;
     private final StatusSignal<Temperature> rollerTemp;
+
+    private final VoltageOut extensionVoltage;
+    private final VelocityVoltage extensionVelocityVoltage;
+    private final PositionVoltage extensionPositionVoltage;
+
+    private final VoltageOut rollerVoltage;
+    private final VelocityVoltage rollerVelocityVoltage;
+
+    private boolean isHomed = false;
+
+    public IntakeIOTalonFX() {
+
+        // for both of these we need canbus id
+        left_motor = new TalonFX(IntakeConstants.EXTENSION_LEFT_MOTOR_ID);
+        right_motor = new TalonFX(IntakeConstants.EXTENSION_RIGHT_MOTOR_ID);
+        intake_motor = new TalonFX(IntakeConstants.ROLLER_MOTOR_ID);
+
+        var extensionConfig = new TalonFXConfiguration();
+        extensionConfig.CurrentLimits.SupplyCurrentLimit = IntakeConstants.EXTENSION_CURRENT_LIMIT.getAsDouble();
+        extensionConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+        extensionConfig.MotorOutput = new MotorOutputConfigs()
+                .withInverted(InvertedValue.CounterClockwise_Positive)
+                .withNeutralMode(NeutralModeValue.Brake);
+        extensionConfig.Feedback.SensorToMechanismRatio = IntakeConstants.EXTENSION_GEAR_RATIO.getAsDouble();
+
+        var extensionSlot0Configs = new Slot0Configs();
+        extensionSlot0Configs
+                .withKA(IntakeConstants.EXTENSION_POSITION_KA.getAsDouble())
+                .withKP(IntakeConstants.EXTENSION_POSITION_KP.getAsDouble())
+                .withKI(IntakeConstants.EXTENSION_POSITION_KI.getAsDouble())
+                .withKD(IntakeConstants.EXTENSION_POSITION_KD.getAsDouble())
+                .withKS(IntakeConstants.EXTENSION_POSITION_KS.getAsDouble())
+                .withKV(IntakeConstants.EXTENSION_POSITION_KV.getAsDouble())
+                .withKG(IntakeConstants.EXTENSION_POSITION_KG.getAsDouble());
+        var extensionSlot1Configs = new Slot1Configs();
+        extensionSlot1Configs
+                .withKA(IntakeConstants.EXTENSION_VELOCITY_KA.getAsDouble())
+                .withKA(IntakeConstants.EXTENSION_VELOCITY_KA.getAsDouble())
+                .withKA(IntakeConstants.EXTENSION_VELOCITY_KA.getAsDouble())
+                .withKA(IntakeConstants.EXTENSION_VELOCITY_KA.getAsDouble())
+
+    }
 
 
 }
