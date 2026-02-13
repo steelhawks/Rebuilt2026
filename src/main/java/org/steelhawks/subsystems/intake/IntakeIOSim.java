16f86e82166da29e732a1a8585e704350f0370ca
Adam M Aptowitz <64217755+Pugga12@users.noreply.github.com>
2026-02-13 18:08:23 -0500

    Attempted intake sim with rack and pinion

---
+++ src/main/java/org/steelhawks/subsystems/intake/IntakeIOSim.java
@@ -0,0 +1,175 @@
+package org.steelhawks.subsystems.intake;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import org.ironmaple.simulation.IntakeSimulation;
+import org.steelhawks.Constants;
+import org.steelhawks.subsystems.swerve.Swerve;
+
+import static edu.wpi.first.units.Units.Inches;
+
+public class IntakeIOSim implements IntakeIO {
+    private final IntakeSimulation intakeSimulation;
+    private final DCMotorSim leftMotorSim;
+    private final DCMotorSim rightMotorSim;
+    private final DCMotorSim intakeMotorSim;
+
+    private static final double RADIUS = IntakeConstants.METERS_PER_ROTATION / (2*Math.PI);
+    private double feedforwardCurrent = 0;
+    private double goalPosition = 0;
+
+    private final PIDController rackPidController;
+    private boolean enablePid;
+
+    public IntakeIOSim() {
+        intakeSimulation = IntakeSimulation.OverTheBumperIntake(
+            "Fuel",
+            Swerve.getDriveSimulation(),
+            Inches.of(22),
+            Inches.of(10.5),
+            IntakeSimulation.IntakeSide.BACK,
+            30
+        );
+
+        leftMotorSim = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                DCMotor.getKrakenX44Foc(1),
+                0.001,
+                IntakeConstants.REDUCTION
+            ),
+            DCMotor.getKrakenX44Foc(1)
+        );
+
+        rightMotorSim = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                DCMotor.getKrakenX44Foc(1),
+                0.001,
+                IntakeConstants.REDUCTION
+            ),
+            DCMotor.getKrakenX44Foc(1)
+        );
+
+        intakeMotorSim = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                DCMotor.getKrakenX44Foc(1),
+                0.001,
+                1
+            ),
+            DCMotor.getKrakenX44Foc(1)
+        );
+
+        rackPidController = new PIDController(
+            IntakeConstants.kP.get(),
+            IntakeConstants.kI.get(),
+            IntakeConstants.kD.get()
+        );
+    }
+
+    @Override
+    public void updateInputs(IntakeIOInputs inputs) {
+        leftMotorSim.update(Constants.UPDATE_LOOP_DT);
+        intakeMotorSim.update(Constants.UPDATE_LOOP_DT);
+        rightMotorSim.update(Constants.UPDATE_LOOP_DT);
+
+        inputs.leftConnected = true;
+        inputs.leftPositionMeters = leftMotorSim.getAngularPositionRad() * IntakeConstants.METERS_PER_ROTATION;
+        inputs.leftVelocityMetersPerSec = leftMotorSim.getAngularVelocityRadPerSec() * RADIUS;
+        inputs.leftAppliedVolts = leftMotorSim.getInputVoltage();
+        inputs.leftCurrentAmps = leftMotorSim.getCurrentDrawAmps();
+        inputs.leftTorqueCurrentAmps = leftMotorSim.getCurrentDrawAmps();
+        inputs.leftTempCelsius = leftMotorSim.getCurrentDrawAmps() * 0.1;
+
+        inputs.rightConnected = true;
+        inputs.rightPositionMeters = rightMotorSim.getAngularPositionRad() * IntakeConstants.METERS_PER_ROTATION;
+        inputs.rightVelocityMetersPerSec = rightMotorSim.getAngularVelocityRadPerSec() * RADIUS;
+        inputs.rightAppliedVolts = rightMotorSim.getInputVoltage();
+        inputs.rightCurrentAmps = rightMotorSim.getCurrentDrawAmps();
+        inputs.rightTorqueCurrentAmps = rightMotorSim.getCurrentDrawAmps();
+        inputs.rightTempCelsius = rightMotorSim.getCurrentDrawAmps() * 0.1;
+        inputs.intakeConnected = true;
+
+        inputs.intakePositionRad = new Rotation2d(intakeMotorSim.getAngularPositionRad());
+        inputs.intakeVelocityRadPerSec = intakeMotorSim.getAngularVelocityRadPerSec();
+        inputs.intakeAppliedVolts = intakeMotorSim.getInputVoltage();
+        inputs.intakeCurrentAmps = intakeMotorSim.getCurrentDrawAmps();
+        inputs.intakeTorqueCurrentAmps = intakeMotorSim.getCurrentDrawAmps();
+        inputs.intakeTempCelsius = intakeMotorSim.getCurrentDrawAmps() * 0.1;
+
+        if (enablePid) {
+            double currentPosition = inputs.leftPositionMeters;
+            double pidOutput = rackPidController.calculate(currentPosition, goalPosition);
+            double commandedCurrent = pidOutput + feedforwardCurrent;
+            double volts = commandedCurrent * DCMotor.getKrakenX44Foc(1).rOhms * leftMotorSim.getAngularVelocityRadPerSec() * DCMotor.getKrakenX44Foc(1).KtNMPerAmp;
+
+            leftMotorSim.setInputVoltage(volts);
+            rightMotorSim.setInputVoltage(volts);
+        }
+    }
+
+    @Override
+    public void setBrakeMode(boolean enabled) {
+        IntakeIO.super.setBrakeMode(enabled);
+    }
+
+    @Override
+    public void runRackPosition(double positionMeters, double feedforward) {
+        goalPosition = positionMeters;
+        feedforwardCurrent = feedforward;
+        enablePid = true;
+    }
+
+    @Override
+    public void runRackOpenLoop(double output, boolean isTorqueCurrent) {
+        if (isTorqueCurrent) {
+            double omega = intakeMotorSim.getAngularVelocityRadPerSec();
+            double volts = output * DCMotor.getKrakenX44Foc(1).rOhms * leftMotorSim.getAngularVelocityRadPerSec() * DCMotor.getKrakenX44Foc(1).KtNMPerAmp;
+
+            leftMotorSim.setInputVoltage(volts);
+            rightMotorSim.setInputVoltage(volts);
+        } else {
+            leftMotorSim.setInputVoltage(output);
+            rightMotorSim.setInputVoltage(output);
+        }
+    }
+
+    @Override
+    public void runRackPercentOut(double output) {
+        leftMotorSim.setInputVoltage(output * 12);
+        rightMotorSim.setInputVoltage(output * 12);
+    }
+
+    @Override
+    public void setRackPID(double kP, double kI, double kD) {
+        rackPidController.setPID(kP, kI, kD);
+    }
+
+    @Override
+    public void setPosition(double meters) {
+        double angle = Units.rotationsToRadians(meters / IntakeConstants.METERS_PER_ROTATION);
+        leftMotorSim.setAngle(angle);
+        rightMotorSim.setAngle(angle);
+    }
+
+    @Override
+    public void runIntake(double output) {
+        intakeMotorSim.setInputVoltage(output * 12);
+        if (output != 0) intakeSimulation.startIntake();
+    }
+
+    @Override
+    public void stopRack() {
+        leftMotorSim.setInputVoltage(0);
+        rightMotorSim.setInputVoltage(0);
+        enablePid = false;
+    }
+
+    @Override
+    public void stopIntake() {
+        intakeSimulation.stopIntake();
+        intakeMotorSim.setInputVoltage(0);
+    }
+}
