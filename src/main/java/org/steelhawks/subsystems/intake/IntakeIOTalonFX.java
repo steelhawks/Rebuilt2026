92884c5718c0f2370278897987174f1ab323903d
krishp <parmar1coding@gmail.com>
2026-02-13 21:00:36 -0500

    Finished UpdateInputs for IntakeIOTalonFX

--- src/main/java/org/steelhawks/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/org/steelhawks/subsystems/intake/IntakeIOTalonFX.java
@@ -1,5 +1,6 @@
 package org.steelhawks.subsystems.intake;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -14,6 +15,7 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.units.measure.*;
+import org.steelhawks.util.PhoenixUtil;
 
 
 public class IntakeIOTalonFX implements IntakeIO {
@@ -56,6 +58,13 @@ public class IntakeIOTalonFX implements IntakeIO {
         left_motor = new TalonFX(IntakeConstants.EXTENSION_LEFT_MOTOR_ID);
         right_motor = new TalonFX(IntakeConstants.EXTENSION_RIGHT_MOTOR_ID);
         intake_motor = new TalonFX(IntakeConstants.ROLLER_MOTOR_ID);
+
+        extensionVoltage = new VoltageOut(0).withEnableFOC(true);
+        extensionVelocityVoltage = new VelocityVoltage(0).withEnableFOC(true);
+        extensionPositionVoltage = new PositionVoltage(0).withEnableFOC(true);
+
+        rollerVoltage = new VoltageOut(0).withEnableFOC(true);
+        rollerVelocityVoltage = new VelocityVoltage(0).withEnableFOC(true);
 
         left_motor.setControl(new Follower(IntakeConstants.EXTENSION_RIGHT_MOTOR_ID, MotorAlignmentValue.Opposed));
 
@@ -110,11 +119,73 @@ public class IntakeIOTalonFX implements IntakeIO {
 
         intake_motor.getConfigurator().apply(rollerSlot0Configs);
 
+        rightExtensionPosition = right_motor.getPosition();
+        rightExtensionAppliedVoltage = right_motor.getMotorVoltage();
+        rightExtensionVelocityPerSec = right_motor.getVelocity();
+        rightExtensionCurrentAmps = right_motor.getSupplyCurrent();
+        rightExtensionTemp = right_motor.getDeviceTemp();
 
+        leftExtensionPosition = left_motor.getPosition();
+        leftExtensionAppliedVoltage = left_motor.getMotorVoltage();
+        leftExtensionVelocityPerSec = left_motor.getVelocity();
+        leftExtensionCurrentAmps = left_motor.getSupplyCurrent();
+        leftExtensionTemp = left_motor.getDeviceTemp();
+
+        rollerPosition = intake_motor.getPosition();
+        rollerAppliedVoltage = intake_motor.getMotorVoltage();
+        rollerVelocityPerSec = intake_motor.getVelocity();
+        rollerCurrentAmps = intake_motor.getSupplyCurrent();
+        rollerTemp = intake_motor.getDeviceTemp();
+
+        BaseStatusSignal.setUpdateFrequencyForAll(50,
+                rightExtensionPosition,
+                rightExtensionAppliedVoltage,
+                rightExtensionTemp,
+                rightExtensionCurrentAmps,
+                rightExtensionVelocityPerSec,
+
+                leftExtensionAppliedVoltage,
+                leftExtensionPosition,
+                leftExtensionCurrentAmps,
+                leftExtensionTemp,
+                leftExtensionVelocityPerSec,
+
+                rollerTemp,
+                rollerAppliedVoltage,
+                rollerCurrentAmps,
+                rollerPosition,
+                rollerVelocityPerSec
+                );
+
+        right_motor.optimizeBusUtilization();
+        left_motor.optimizeBusUtilization();
+        intake_motor.optimizeBusUtilization();
+    }
+
+    @Override
+    public void updateInputs(IntakeIOInputs inputs) {
+        inputs.leftConnected = BaseStatusSignal.isAllGood(leftExtensionTemp, leftExtensionPosition, leftExtensionCurrentAmps, leftExtensionAppliedVoltage, leftExtensionVelocityPerSec);
+        inputs.leftExtensionPosition = leftExtensionPosition.getValueAsDouble() * IntakeConstants.PINION_ROTATION;
+        inputs.leftExtensionVelocity = leftExtensionVelocityPerSec.getValueAsDouble() * IntakeConstants.PINION_ROTATION;
+        inputs.leftExtensionAppliedVolts = leftExtensionAppliedVoltage.getValueAsDouble();
+        inputs.leftExtensionCurrentAmps = leftExtensionCurrentAmps.getValueAsDouble();
+        inputs.leftExtensionTempCelsius = leftExtensionTemp.getValueAsDouble();
+
+        inputs.rightConnected = BaseStatusSignal.isAllGood(rightExtensionTemp, rightExtensionCurrentAmps, rightExtensionVelocityPerSec, rightExtensionPosition, rightExtensionAppliedVoltage);
+        inputs.rightExtensionPosition = rightExtensionPosition.getValueAsDouble() * IntakeConstants.PINION_ROTATION;
+        inputs.rightExtensionVelocity = rightExtensionVelocityPerSec.getValueAsDouble() * IntakeConstants.PINION_ROTATION;
+        inputs.rightExtensionAppliedVolts = rightExtensionAppliedVoltage.getValueAsDouble();
+        inputs.rightExtensionCurrentAmps = rightExtensionCurrentAmps.getValueAsDouble();
+        inputs.rightExtensionTempCelsius = leftExtensionTemp.getValueAsDouble();
+
+        inputs.isExtended = inputs.rightExtensionPosition >= IntakeConstants.MAX_EXTENSION - IntakeConstants.POSITION_TOLERANCE;
+        inputs.isRetracted = inputs.rightExtensionPosition <= IntakeConstants.MAX_EXTENSION + IntakeConstants.POSITION_TOLERANCE;
 
 
 
     }
+
+
 
 
 }
