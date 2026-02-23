aaf4dd4695f0c9a5771c7b66a0fe36f4e4f72b3c
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2026-02-22 22:24:58 -0500

    Revert "Merge branch 'feat/hood-sim' into new-intakesim-with-rack"
    This reverts commit 52eebd9b6ba3d88eb6706ab8aee4ed3f05b1276f, reversing
    changes made to b9095d0d39f8d4864b4a9aba5ac0540992e4fc59.

--- src/main/java/org/steelhawks/subsystems/superstructure/turret/TurretVisualizer.java
+++ src/main/java/org/steelhawks/subsystems/superstructure/turret/TurretVisualizer.java
@@ -1,18 +1,56 @@
 package org.steelhawks.subsystems.superstructure.turret;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import org.steelhawks.subsystems.superstructure.SuperstructureVisualizer;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+import org.steelhawks.Constants;
 
 import java.util.function.Supplier;
 
 public class TurretVisualizer {
 
-    public TurretVisualizer(Supplier<Double> turretRad, Supplier<Pose2d> robotPose) {
-        SuperstructureVisualizer.getInstance().setTurretSupplier(turretRad);
-        SuperstructureVisualizer.getInstance().setRobotPoseSupplier(robotPose);
-    }
+	private final LoggedMechanism2d turret;
+	private final LoggedMechanismLigament2d shooter;
+	private final Supplier<Double> turretPositionSupplier;
+	private final Supplier<Pose2d> robotPositionSupplier;
+	private Pose2d turretPose;
+
+    private static final Translation2d TURRET_OFFSET_2D = new Translation2d(
+        Constants.RobotConstants.ROBOT_TO_TURRET.getX(),
+        Constants.RobotConstants.ROBOT_TO_TURRET.getY()
+    );
+
+	public TurretVisualizer(Supplier<Double> turretPosition, Supplier<Pose2d> robotPose) {
+		this.turretPositionSupplier = turretPosition;
+		this.robotPositionSupplier = robotPose;
+		turret = new LoggedMechanism2d(3, 3);
+		var root = turret.getRoot("TurretCenter", 1.5, 1.5);
+		shooter = new LoggedMechanismLigament2d(
+			"Turret", 1, Math.toDegrees(turretPosition.get()) + 90, 5, new Color8Bit(Color.kWhite));
+		root.append(shooter);
+		turretPose = new Pose2d(
+			robotPose.get().getTranslation().plus(TURRET_OFFSET_2D),
+			new Rotation2d(turretPositionSupplier.get() + Math.PI / 2));
+	}
 
     public void update() {
-        SuperstructureVisualizer.getInstance().update();
+        shooter.setAngle(new Rotation2d(turretPositionSupplier.get() + Math.PI / 2));
+        Pose2d robot = robotPositionSupplier.get();
+        Translation2d turretWorld = robot.getTranslation()
+            .plus(TURRET_OFFSET_2D.rotateBy(robot.getRotation()));
+        turretPose = new Pose2d(
+            turretWorld,
+            robot.getRotation().plus(new Rotation2d(turretPositionSupplier.get())));
+        Logger.recordOutput("Turret/Mechanism2d", turret);
+        Logger.recordOutput("Turret/TurretPose", turretPose);
+        Logger.recordOutput("Turret/ComponentPoses", new Pose3d[] {
+            new Pose3d(
+                Constants.RobotConstants.ROBOT_TO_TURRET.getTranslation(),
+                new Rotation3d(0.0, 0.0, turretPositionSupplier.get() + Math.PI)
+            )
+        });
     }
 }
