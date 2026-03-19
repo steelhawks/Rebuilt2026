5014fe1146fef3f9a90da52e182f5d75990ad939
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2026-03-19 12:17:47 -0400

    Update at 'Thu Mar 19 12:17:46 EDT 2026'

--- src/main/java/org/steelhawks/RobotContainer.java
+++ src/main/java/org/steelhawks/RobotContainer.java
@@ -3,6 +3,7 @@ package org.steelhawks;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import org.steelhawks.commands.*;
 import org.steelhawks.subsystems.intake.Intake;
 import org.steelhawks.subsystems.intake.IntakeConstants;
@@ -78,8 +79,15 @@ public class RobotContainer {
 //                    .andThen(s_Intake.runIntake())
 //                    .finallyDo(() -> s_Intake.setDesiredState(IntakeConstants.State.HOME)));
 //        }
-//        driver.rightTrigger()
-//            .whileTrue();
+        driver.rightTrigger()
+            .whileTrue(
+                s_Intake.runIntake());
+
+        driver.x()
+            .onTrue(s_Intake.slamOut());
+
+        driver.y()
+            .onTrue(s_Intake.slamOut());
 
 //        if (config.hasTurret) {
 //            driver.x()
