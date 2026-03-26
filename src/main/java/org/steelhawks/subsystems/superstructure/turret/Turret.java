895ee2477c56c16de017ec6a95124a93ae97ba41
krishp <parmar1coding@gmail.com>
2026-03-26 18:34:20 -0400

    Started Turret.java

--- src/main/java/org/steelhawks/subsystems/superstructure/turret/Turret.java
+++ src/main/java/org/steelhawks/subsystems/superstructure/turret/Turret.java
@@ -1,5 +1,125 @@
 package org.steelhawks.subsystems.superstructure.turret;
 
-public class Turret {
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+import org.steelhawks.BuilderConstants;
+import org.steelhawks.Toggles;
+import org.steelhawks.subsystems.superstructure.flywheel.FlywheelIOInputsAutoLogged;
+import org.steelhawks.util.LoggedTunableNumber;
+
+import java.util.function.Supplier;
+
+public class Turret extends SubsystemBase {
+    private TurretIO io;
+    private TurretIOInputsAutoLogged inputs;
+    private TurretState turretState = TurretState.IDLE;
+
+    private static final double POSITION_TOLERANCE_ROTATIONS = 0.01;
+
+    private TrapezoidProfile profile;
+    private TrapezoidProfile.State goal =  new TrapezoidProfile.State();
+    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
+
+    private static LoggedTunableNumber kP;
+    private static LoggedTunableNumber kI;
+    private static LoggedTunableNumber kD;
+    private static LoggedTunableNumber kS;
+    private static LoggedTunableNumber kA;
+
+    private final LoggedTunableNumber maxVelocity;
+    private final LoggedTunableNumber maxAcceleration;
+    private final LoggedTunableNumber currentThreshold;
+
+    private Rotation2d desiredRotation = new Rotation2d();
+    private double homingVolts = 0.1;
+    private Supplier<Pose2d> poseSupplier;
+
+    private BuilderConstants.TurretConstants constants;
+
+    private LoggedTunableNumber tuningVolts;
+    private LoggedTunableNumber tuningAmps;
+
+
+    enum TurretState {
+        IDLE,
+        HOMED,
+        ROTATING,
+        ZEROED,
+    }
+
+    public Turret(TurretIO io, Supplier<Pose2d> poseSupplier, BuilderConstants.TurretConstants constants) {
+        this.io = io;
+        this.constants = constants;
+        this.poseSupplier = poseSupplier;
+
+        kP = new LoggedTunableNumber("Turret/kP", constants.kP());
+        kI = new LoggedTunableNumber("Turret/kI", constants.kI());
+        kD = new LoggedTunableNumber("Turret/kD", constants.kD());
+        kS = new LoggedTunableNumber("Turret/kS", constants.kS());
+        kA = new LoggedTunableNumber("Turret/kA", constants.kA());
+
+        inputs = new TurretIOInputsAutoLogged();
+
+        maxAcceleration = new LoggedTunableNumber("Turret/maxAcceleration", constants.maxAccelerationRadPerSecSq());
+        maxVelocity = new LoggedTunableNumber("Turret/MaxVelocity", constants.maxVelocityRadPerSec());
+        currentThreshold = new LoggedTunableNumber("Turret/CurrentHomingThreshold", 4);
+
+        profile = buildProfile();
+
+    }
+
+    private TrapezoidProfile buildProfile() {
+        return new TrapezoidProfile(
+                new TrapezoidProfile.Constraints(maxVelocity.get(), maxAcceleration.get())
+        );
+    }
+
+    public boolean atGoal() {
+        return Math.abs(inputs.position.getRotations() - goal.position) < POSITION_TOLERANCE_ROTATIONS;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Turret", inputs);
+        Logger.recordOutput("Turret/State", turretState.toString());
+
+        final boolean shouldRun = Toggles.Turret.isEnabled.get();
+
+        if (Toggles.tuningMode.getAsBoolean()) {
+
+            LoggedTunableNumber.ifChanged(
+                    hashCode(), () -> io.setTurretPID(kP.get(), kI.get(), kD.get()), kP, kI, kD
+            );
+
+            LoggedTunableNumber.ifChanged(
+                    hashCode(), () -> profile = buildProfile(), maxVelocity, maxAcceleration
+            );
+
+            if (Toggles.Turret.toggleVoltageOverride.getAsBoolean()) {
+                if (tuningVolts == null) {
+                    tuningVolts = new LoggedTunableNumber("Turret/TuningVolts", 0.0);
+                }
+                io.runOpenLoop(tuningVolts.getAsDouble(), false);
+            }
+
+            if (Toggles.Turret.toggleCurrentOverride.getAsBoolean()) {
+                if (tuningAmps == null) {
+                    tuningAmps = new LoggedTunableNumber("Turret/TuningAmps", 0.0);
+                }
+                io.runOpenLoop(tuningAmps.getAsDouble(), false);
+            }
+        }
+
+        if (shouldRun) {
+
+        } else {
+            io.stopTurret();
+            return;
+        }
+    }
 
 }
--- src/main/java/org/steelhawks/subsystems/superstructure/turret/TurretIOSim.java
+++ src/main/java/org/steelhawks/subsystems/superstructure/turret/TurretIOSim.java
@@ -1,4 +1,107 @@
 package org.steelhawks.subsystems.superstructure.turret;
 
-public class TurretIOSim {
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import org.steelhawks.BuilderConstants;
+import org.steelhawks.Constants;
+
+public class TurretIOSim implements TurretIO {
+    private final PIDController pidController;
+    private final DCMotorSim turretMotor;
+
+    // private final TurretVisualizer turretVisualizer
+
+    private double feedfoward = 0.0;
+    private double goalPosition = 0.0;
+
+    private double MAX_VOLTS = 12.0;
+
+    private boolean enablePID = false;
+
+    public TurretIOSim(BuilderConstants.TurretConstants constants) {
+        turretMotor = new DCMotorSim(
+                LinearSystemId.createDCMotorSystem(
+                DCMotor.getKrakenX60Foc(1),
+                0.001,
+                constants.motorReduction()
+        ),
+                DCMotor.getKrakenX60Foc(1)
+                );
+        pidController = new PIDController(
+                constants.kP(),
+                constants.kI(),
+                constants.kD()
+        );
+
+        /*   turretVisualizer = new TurretVisualizer(
+            mMotor::getAngularPositionRad,
+            () -> RobotState.getInstance().getEstimatedPose()); */
+
+    }
+
+    DCMotor motor = DCMotor.getKrakenX60Foc(1);
+
+    public double currentToTorqueVolts(double current) {
+        double torque = motor.KtNMPerAmp * current;
+
+        double voltage = current * motor.rOhms
+                + turretMotor.getAngularVelocityRadPerSec() / motor.KvRadPerSecPerVolt;
+        return voltage;
+    }
+
+    @Override
+    public void updateInputs(TurretIOInputs inputs) {
+
+        turretMotor.update(Constants.UPDATE_LOOP_DT);
+
+        inputs.isConnected = true;
+        inputs.position = new Rotation2d(turretMotor.getAngularPositionRad());
+        inputs.velocityRadPerSec = new Rotation2d(turretMotor.getAngularVelocityRadPerSec());
+        inputs.appliedVolts = turretMotor.getInputVoltage();
+        inputs.currentAmps = turretMotor.getCurrentDrawAmps();
+        inputs.torqueCurrent = turretMotor.getCurrentDrawAmps();
+        inputs.statorCurrent = turretMotor.getCurrentDrawAmps();
+        inputs.tempCelsius = turretMotor.getCurrentDrawAmps() * 0.1;
+
+        if (enablePID) {
+            double pid = pidController.calculate(inputs.position.getRadians(), goalPosition);
+            turretMotor.setInputVoltage(currentToTorqueVolts(pid + feedfoward));
+        }
+    }
+
+    @Override
+    public void setTurretPosition(double position) {
+        goalPosition = position;
+        turretMotor.setAngle(position);
+    }
+
+    @Override
+    public void runOpenLoop(double output, boolean isTorqueCurrent) {
+        if (isTorqueCurrent) {
+            turretMotor.setInputVoltage(currentToTorqueVolts(output));
+        } else {
+            turretMotor.setInputVoltage(output);
+        }
+    }
+
+    @Override
+    public void runTurret(double output) {
+        turretMotor.setInputVoltage(output);
+    }
+
+    @Override
+    public void stopTurret() {
+        turretMotor.setInputVoltage(0.0);
+    }
+
+    @Override
+    public void setTurretPID(double kP, double kI, double kD) {
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+    }
+
 }
