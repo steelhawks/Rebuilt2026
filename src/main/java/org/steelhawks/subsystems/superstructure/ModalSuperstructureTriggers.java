e4d45f0d736b03e267384a5befce46cb9f18f16c
krishp <parmar1coding@gmail.com>
2026-04-24 13:52:16 -0400

    superstructure state finished

--- src/main/java/org/steelhawks/subsystems/superstructure/ModalSuperstructureTriggers.java
+++ src/main/java/org/steelhawks/subsystems/superstructure/ModalSuperstructureTriggers.java
@@ -1,4 +1,99 @@
 package org.steelhawks.subsystems.superstructure;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.*;
+import org.steelhawks.FieldConstants;
+import org.steelhawks.RobotContainer;
+import org.steelhawks.RobotState;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class ModalSuperstructureTriggers {
+    private final RobotContainer robotContainer;
+    private final SSM stateMachine;
+    private final FuelStateTracker fuelTracker;
+
+    private final AtomicReference<SuperstructureState> latestFuelState = new AtomicReference<>(
+            SuperstructureState.IN_HOPPER
+    );
+    private final AtomicBoolean coralAtShooter =  new AtomicBoolean(false);
+    private final AtomicReference<RobotState.AimState> selectedAimstate = new AtomicReference<>(RobotState.AimState.TO_HUB);
+    private final AtomicBoolean enableAutoStow = new AtomicBoolean(false);
+    private final AtomicBoolean enableHubSnap =  new AtomicBoolean(true);
+
+    public ModalSuperstructureTriggers(RobotContainer robotContainer, SSM stateMachine, FuelStateTracker fuelTracker) {
+        this.robotContainer = robotContainer;
+        this.stateMachine = stateMachine;
+        this.fuelTracker = fuelTracker;
+    }
+
+    public AtomicReference<SuperstructureState> getLatestFuelState() {
+        return latestFuelState;
+    }
+
+    private boolean isScoreableFuel(SuperstructureState fuelState) {
+        return fuelState == SuperstructureState.SPINDEXING
+                || fuelState == SuperstructureState.SHOOTING
+                || fuelState == SuperstructureState.IN_HOPPER;
+    }
+
+    private boolean isCloseToHub(Pose2d pose) {
+        Translation2d robotTranslation = pose.getTranslation();
+        double distanceToHub =  FieldConstants.Hub.HUB_CENTER.getDistance(robotTranslation);
+        return distanceToHub < 0.3;
+    }
+
+    private Command setStateCommand(SuperstructureState state, String name) {
+        return Commands.none();
+    }
+
+    private Command setStateCommand(SuperstructureState state, boolean setFuture, String name) {
+        return Commands.none();
+    }
+
+    private Command setStateCommand(AtomicReference<SuperstructureState> state, boolean setFuture, String name) {
+        return Commands.none();
+    }
+
+    public Command setAndWaitCommand(SuperstructureState state, String name) {
+        return Commands.sequence(
+                setStateCommand(state, name),
+                new WaitUntilCommand(() -> stateMachine != null)
+        );
+    }
+
+    public Command setFutureStateAndWaitCommand(SuperstructureState state, String name) {
+        return Commands.sequence(
+                setStateCommand(state, true, name),
+                new WaitUntilCommand(() -> stateMachine != null)
+        );
+    }
+
+    public Command commandToDesiredStateCommand() {
+        return Commands.defer(
+                () -> setAndWaitCommand(null, "Stage Desired State"),
+                Set.of());
+    }
+
+    private Command createScoreHubCommand() {
+        Command indexCommand = commandToDesiredStateCommand();
+        Command scoreCommand = new InstantCommand(Commands::none);
+        Command fullScoreSequence = Commands.sequence(indexCommand, scoreCommand);
+        return new ConditionalCommand(
+                fullScoreSequence,
+                Commands.none(),
+                () -> isScoreableFuel(null)).withName("HI");
+    }
+
+    private  Command createHubSnapCommand() {
+
+    }
+
+
+
+
+
 }
--- src/main/java/org/steelhawks/subsystems/superstructure/SuperstructureState.java
+++ src/main/java/org/steelhawks/subsystems/superstructure/SuperstructureState.java
@@ -1,12 +1,15 @@
 package org.steelhawks.subsystems.superstructure;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import org.steelhawks.RobotContainer;
-import org.steelhawks.RobotState;
+import edu.wpi.first.wpilibj2.command.Commands;
+import org.steelhawks.*;
+import org.steelhawks.commands.ShootingCommands;
+import org.steelhawks.subsystems.beam.BeamIOInputsAutoLogged;
 import org.steelhawks.subsystems.intake.IntakeConstants;
-import org.steelhawks.subsystems.intake.MagicIntake;
-import org.steelhawks.subsystems.shooterSuperstructure.ShooterStructure;
 
+import java.util.EnumSet;
+import java.util.Set;
 import java.util.function.Function;
 
 public enum SuperstructureState {
@@ -29,13 +32,72 @@ public enum SuperstructureState {
                 }
                 return moveCommand;
             }
+    ),
+    SPINDEXING(
+      container -> {
+          Command runCommand = null;
+          BeamIOInputsAutoLogged inputs = new  BeamIOInputsAutoLogged();
+          if (inputs.connected && inputs.detected) {
+                if (container.s_Flywheel.isReadyToShoot() && container.s_Turret.atGoal() && container.s_Hood.atGoal()) {
+                    runCommand = ShootingCommands.shoot();
+                } else {
+                    runCommand = container.s_Indexer.feed();
+                }
+          }
+          return runCommand;
+    }),
+    SHOOTING(
+            container -> {
+                Command runCommand = null;
+                if (RobotState.getInstance().getAimState() == RobotState.AimState.FERRY) {
+                    runCommand = ShootingCommands.shoot();
+                } else if (RobotState.getInstance().getAimState() == RobotState.AimState.TO_HUB) {
+                    runCommand = ShootingCommands.shootWhileIntaking();
+                }
+                return runCommand;
+            }
+    ),
+    IN_HOPPER(
+            container -> {
+                return Commands.none();
+            }
     );
-
-
-
     private final Function<RobotContainer, Command>  commandSupplier;
 
     SuperstructureState(Function<RobotContainer, Command> commandSupplier) {
         this.commandSupplier = commandSupplier;
     }
+
+
+    public Command getCommand(RobotContainer container) {
+        if (commandSupplier != null) {
+            return Commands.none();
+        }
+        return commandSupplier.apply(container);
+    }
+
+
+    public Set<SuperstructureState> allowedNextStates() {
+        Set<SuperstructureState> all = EnumSet.allOf(SuperstructureState.class);
+        switch (this) {
+            case INTAKING, SPINDEXING:
+                all.remove(EnumSet.of(STOWED));
+                return all;
+            case STOWED:
+                all.remove(EnumSet.of(INTAKING, SHOOTING, SPINDEXING));
+                return all;
+            case SHOOTING:
+                all.remove(EnumSet.of(STOWED, INTAKING));
+                return all;
+            default:
+                return all;
+
+        }
+    }
+
+    public double getAutoAlignHub(Pose2d pose) {
+        return FieldConstants.Hub.HUB_CENTER.getDistance(pose.getTranslation());
+    }
+
+
 }
