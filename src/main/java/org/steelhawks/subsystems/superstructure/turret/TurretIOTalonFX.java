3bee3165d3fbc0e8a5ffe4036cc0668d1a4270cb
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2026-04-30 15:23:55 -0500

    Update at 'Thu Apr 30 15:23:54 CDT 2026'

--- src/main/java/org/steelhawks/subsystems/superstructure/turret/TurretIOTalonFX.java
+++ src/main/java/org/steelhawks/subsystems/superstructure/turret/TurretIOTalonFX.java
@@ -66,10 +66,10 @@ public class TurretIOTalonFX implements TurretIO {
         motorConfig.Slot1.kS = 25.0;
 
         motorConfig.ClosedLoopGeneral.ContinuousWrap = false;
-        motorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+        motorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
         motorConfig.Feedback.FeedbackRemoteSensorID = encoder.getDeviceID();
         motorConfig.Feedback.RotorToSensorRatio = constants.motorReduction();
-        motorConfig.Feedback.SensorToMechanismRatio = 6.0 / 7.0;
+        motorConfig.Feedback.SensorToMechanismRatio = 7.0 / 6.0;
         motorConfig.CurrentLimits.SupplyCurrentLimit = CurrentLimits.SupplyLimit.turretCurrent;
         motorConfig.CurrentLimits.SupplyCurrentLimitEnable = CurrentLimits.SupplyLimit.turretEnabled;
         motorConfig.CurrentLimits.StatorCurrentLimit = CurrentLimits.StatorLimit.turretCurrent;
